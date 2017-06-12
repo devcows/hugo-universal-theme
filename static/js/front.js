4730379a2ceea4f419271f0333e33bb80e51240a
Guillermo Guerrero <wolf.fox1985@gmail.com>
2017-06-12 11:58:54 +0200

    Fixed travis

--- static/js/front.js
+++ static/js/front.js
@@ -33,8 +33,8 @@ function contactForm () {
         $this[0].reset() // clear form
 
         $('#contact-message')
-        .html('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>Thank you for getting in touch. We will get back to you soon!</div>')
-        .fadeIn()
+          .html('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>Thank you for getting in touch. We will get back to you soon!</div>')
+          .fadeIn()
       }
       , 'json')
     return false
