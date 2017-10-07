a72a3a991f07b9a3d2787f4766a19d3847fbd236
Helena Dobrovolskaya <Helena Dobrovolskaya helena.nevskaya@gmail.com>
2017-10-07 17:23:18 +0300

    translate of contact form message;

--- static/js/front.js
+++ static/js/front.js
@@ -33,7 +33,6 @@ function contactForm () {
         $this[0].reset() // clear form
 
         $('#contact-message')
-          .html('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>Thank you for getting in touch. We will get back to you soon!</div>')
           .fadeIn()
       }
       , 'json')
