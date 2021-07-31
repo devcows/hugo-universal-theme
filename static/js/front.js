9ee8297bf477dacc371da488eb05d1c22b6b4c2f
Guillermo Guerrero Ibarra <guillermo.guerrero@deliveroo.co.uk>
2021-08-01 01:04:02 +0200

    Carousel customizable.

--- static/js/front.js
+++ static/js/front.js
@@ -95,37 +95,31 @@ function sliderHomepage () {
 function sliders () {
   if ($('.owl-carousel').length) {
     $('.customers').owlCarousel({
-      items: 6,
+      items: ($('.customers').attr('data-items') || 6),
+      slideSpeed: ($('.customers').attr('data-slide-speed') || 2000),
+      paginationSpeed: ($('.customers').attr('data-pagination-speed') || 1000),
+      autoPlay: $('.customers').attr('data-autoplay') === 'true',
       itemsDesktopSmall: [990, 4],
       itemsTablet: [768, 2],
       itemsMobile: [480, 1]
     })
 
     $('.testimonials').owlCarousel({
-      items: 4,
+      items: ($('.testimonials').attr('data-items') || 4),
+      slideSpeed: ($('.testimonials').attr('data-slide-speed') || 2000),
+      paginationSpeed: ($('.testimonials').attr('data-pagination-speed') || 1000),
+      autoPlay: $('.testimonials').attr('data-autoplay') === 'true',
       itemsDesktopSmall: [990, 3],
       itemsTablet: [768, 2],
       itemsMobile: [480, 1]
     })
 
-    $('.project').owlCarousel({
-      navigation: true, // Show next and prev buttons
-      navigationText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
-      slideSpeed: 300,
-      paginationSpeed: 400,
-      autoPlay: true,
-      stopOnHover: true,
-      singleItem: true,
-      afterInit: '',
-      lazyLoad: true
-    })
-
     $('.homepage').owlCarousel({
       navigation: false, // Show next and prev buttons
       navigationText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
-      slideSpeed: 2000,
-      paginationSpeed: 1000,
-      autoPlay: true,
+      slideSpeed: ($('.homepage').attr('data-slide-speed') || 2000),
+      paginationSpeed: ($('.homepage').attr('data-pagination-speed') || 1000),
+      autoPlay: ($('.homepage').attr('data-autoplay') || 'true') === 'true',
       stopOnHover: true,
       singleItem: true,
       lazyLoad: false,
