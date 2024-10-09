2dffbffb0b9f724a438234d9b6c8b373143cb69f
ryanfox1985 <ryanfox1985@users.noreply.github.com>
2024-10-09 22:31:05 +0000

    deploy: 37171f71c2380cbbba7962b46916e2258420edeb

--- js/gmaps.init.js
+++ js/gmaps.init.js
@@ -6,38 +6,38 @@ $(document).ready(function () {
 
 function map () {
   if ($('#map').length) {
-    var lat = $('#gmap-lat').val()
-    var lng = $('#gmap-lng').val()
-    var direction = $('#gmap-dir').val()
-    var image = $('#gmap-marker').val()
+    const lat = $('#gmap-lat').val()
+    const lng = $('#gmap-lng').val()
+    const direction = $('#gmap-dir').val()
+    const image = $('#gmap-marker').val()
 
-    var styles =
+    const styles =
       [
         {
-          'featureType': 'landscape', 'stylers': [{'saturation': -100}, {'lightness': 65}, {'visibility': 'on'}]
+          'featureType': 'landscape', 'stylers': [{ 'saturation': -100 }, { 'lightness': 65 }, { 'visibility': 'on' }]
         }, {
-          'featureType': 'poi', 'stylers': [{'saturation': -100}, {'lightness': 51}, {'visibility': 'simplified'}]
+          'featureType': 'poi', 'stylers': [{ 'saturation': -100 }, { 'lightness': 51 }, { 'visibility': 'simplified' }]
         }, {
-          'featureType': 'road.highway', 'stylers': [{'saturation': -100}, {'visibility': 'simplified'}]
+          'featureType': 'road.highway', 'stylers': [{ 'saturation': -100 }, { 'visibility': 'simplified' }]
         }, {
-          'featureType': 'road.arterial', 'stylers': [{'saturation': -100}, {'lightness': 30}, {'visibility': 'on'}]
+          'featureType': 'road.arterial', 'stylers': [{ 'saturation': -100 }, { 'lightness': 30 }, { 'visibility': 'on' }]
         }, {
-          'featureType': 'road.local', 'stylers': [{'saturation': -100}, {'lightness': 40}, {'visibility': 'on'}]
+          'featureType': 'road.local', 'stylers': [{ 'saturation': -100 }, { 'lightness': 40 }, { 'visibility': 'on' }]
         }, {
-          'featureType': 'transit', 'stylers': [{'saturation': -100}, {'visibility': 'simplified'}]
+          'featureType': 'transit', 'stylers': [{ 'saturation': -100 }, { 'visibility': 'simplified' }]
         }, {
-          'featureType': 'administrative.province', 'stylers': [{'visibility': 'off'}]
+          'featureType': 'administrative.province', 'stylers': [{ 'visibility': 'off' }]
         }, {
-          'featureType': 'water', 'elementType': 'labels', 'stylers': [{'visibility': 'on'}, {'lightness': -25}, {'saturation': -100}]
+          'featureType': 'water', 'elementType': 'labels', 'stylers': [{ 'visibility': 'on' }, { 'lightness': -25 }, { 'saturation': -100 }]
         }, {
-          'featureType': 'water', 'elementType': 'geometry', 'stylers': [{'hue': '#ffff00'}, {'lightness': -25}, {'saturation': -97}]
+          'featureType': 'water', 'elementType': 'geometry', 'stylers': [{ 'hue': '#ffff00' }, { 'lightness': -25 }, { 'saturation': -97 }]
         }
       ]
 
-    var map = new GMaps({
+    const map = new GMaps({
       el: '#map',
-      lat: lat,
-      lng: lng,
+      lat,
+      lng,
       zoomControl: true,
       zoomControlOpt: {
         style: 'SMALL',
@@ -49,16 +49,16 @@ function map () {
       overviewMapControl: false,
       scrollwheel: false,
       draggable: false,
-      styles: styles
+      styles
     })
 
     map.addMarker({
-      lat: lat,
-      lng: lng,
+      lat,
+      lng,
       icon: image,
       click: function (e) {
         // when we get an address with spaces ...
-        var url = 'https://maps.google.com?daddr=' + direction.split('match').join('replace')
+        const url = 'https://maps.google.com?daddr=' + direction.split('match').join('replace')
         window.open(url, '_blank')
       },
       title: direction
